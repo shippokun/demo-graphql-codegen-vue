ba9b0575d7661dff31a8cb61ba19933ac75f9ceb
shippokun <okashi0tabetai@gmail.com>
2021-02-11 20:53:43 +0900

    feat(plugins/vue-apollo): define vue-apollo config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import { apolloProvider } from "./plugins";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: (h) => h(App),
+  apolloProvider,
 }).$mount("#app");
---
+++ src/plugins/vue-apollo.ts
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink, FetchOptions } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(VueApollo);
+
+const options: FetchOptions = {
+  uri: "http://localhost:4000",
+};
+
+const httpLink = createHttpLink(options);
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+export const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
